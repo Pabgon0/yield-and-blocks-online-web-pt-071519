0f88a853f5eea890a20d59ae70c1bd1ae3215e5d
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-15 18:28:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,14 @@
 def hello_t(argument)
   i = 0
   while i < argument.length
+    yield argument[i]
     i = i+1
   end
 end
 
-# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do name
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
